29f43ea8200cfaa8c5d071d97cd606dccb3a6f3c
arcadeVault <neoarcadegame0@gmail.com>
2025-04-15 13:06:38 +0530

    amit is adding the changes

--- src/main/java/com/learning_git/learning_git/Testing.java
+++ src/main/java/com/learning_git/learning_git/Testing.java
@@ -3,7 +3,8 @@ package com.learning_git.learning_git;
 public class Testing {
 
   public static void main(String[] args) {
-    System.out.println("hello World");
+
+    System.out.println("Hello Buddy");
   }
   
 }
